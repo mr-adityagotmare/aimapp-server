5f2b542a6fda5baf38396c5782bff31ef9b367c1
Aditya Gotmare <mr.adityagotmare@gmail.com>
2026-05-21 14:28:25 +0530

    Refactor MongoDB URI to use environment variable
    Updated MongoDB connection URI to use environment variable.

--- server.js
+++ server.js
@@ -21,8 +21,7 @@ const PEER_TIMEOUT = 60000;       // 60s
 // The database "aimesig" and collection "profile_pictures" are created
 // automatically on first write.
 
-const MONGO_URI =
-  'mongodb+srv://aimappadmin:StrongPassword123@aimapp-cluster.7rymq5m.mongodb.net/aimesig?retryWrites=true&w=majority&appName=aimapp-cluster';
+const MONGO_URI = process.env.MONGODB_URI || '';
 let profilePicturesCollection = null;
 
 async function connectMongo() {
